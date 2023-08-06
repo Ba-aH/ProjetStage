78f11a9364db6cc384cff0d7d581de8c4d933b3b
Baha <behantous@gmail.com>
2023-08-06 20:08:01 +0100

    update 7

--- Back/utilisateur/serializer.py
+++ Back/utilisateur/serializer.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'name', 'password')
+        fields = ('username', 'name', 'password','email')
